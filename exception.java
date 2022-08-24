81054cc1dc7d3406d379dae5035d652e71bbf632
harsha-karrothu <111970541+harsha-karrothu@users.noreply.github.com>
2022-08-24 15:21:39 +0530

    Add files via upload
    ;m,,. n ljll,;m,  , ..,.kl,l

---
+++ exception.java
@@ -0,0 +1,12 @@
+  class Testex
+  {
+         public static void main (String args[])
+          {
+           System.out.println("main method started ");
+            int a=10,b=0,c;
+            c=(a/b);
+           System.out.print.ln(c);
+           System.out.println("main method ended ");
+          }
+}
+                                 
